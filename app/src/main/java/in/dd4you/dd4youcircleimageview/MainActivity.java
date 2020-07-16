338233859336cd05704f85928424e815a250914c
Vinay Singh <41217230+DD4You@users.noreply.github.com>
2020-07-16 06:55:35 +0530

    Initial commit

---
+++ app/src/main/java/in/dd4you/dd4youcircleimageview/MainActivity.java
@@ -0,0 +1,14 @@
+package in.dd4you.dd4youcircleimageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
